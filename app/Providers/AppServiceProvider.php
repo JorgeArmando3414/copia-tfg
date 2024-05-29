abd2b7d102ebf365c61d36f40345ecef193f5db9
JorgeArmando3414 <119513654+JorgeArmando3414@users.noreply.github.com>
2024-05-29 12:34:15 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -20,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        }
         Password::defaults(function () {
             return Password::min(8);
         });
